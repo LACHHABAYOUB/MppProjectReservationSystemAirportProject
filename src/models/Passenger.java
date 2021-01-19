a03d1705748d25341468e573e42e2d5daa606602
LACHHAB AYOUB <44031876+LACHHABAYOUB@users.noreply.github.com>
2021-01-19 16:53:53 -0600

    Change Passenger.java
    Create the relation and make all instance private .

--- src/models/Passenger.java
+++ src/models/Passenger.java
@@ -1,16 +1,17 @@
 package models;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Passenger {
-	String id ;
-	String FirstName;
-	String LastName;
-	LocalDate DOB ; 
-	String Email;
-	
-	
-	
+	private String id;
+	private String FirstName;
+	private String LastName;
+	private LocalDate DOB;
+	private String Email;
+	private List<Reservation> reservations;
+
 	public Passenger(String id, String firstName, String lastName, LocalDate dOB, String email) {
 		super();
 		this.id = id;
@@ -18,39 +19,49 @@ public class Passenger {
 		LastName = lastName;
 		DOB = dOB;
 		Email = email;
+		reservations = new ArrayList<Reservation>();
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getFirstName() {
 		return FirstName;
 	}
+
 	public void setFirstName(String firstName) {
 		FirstName = firstName;
 	}
+
 	public String getLastName() {
 		return LastName;
 	}
+
 	public void setLastName(String lastName) {
 		LastName = lastName;
 	}
+
 	public LocalDate getDOB() {
 		return DOB;
 	}
+
 	public void setDOB(LocalDate dOB) {
 		DOB = dOB;
 	}
+
 	public String getEmail() {
 		return Email;
 	}
+
 	public void setEmail(String email) {
 		Email = email;
 	}
-	
-	
+
 	@Override
 	public String toString() {
 		return "Passenger [id=" + id + ", FirstName=" + FirstName + ", LastName=" + LastName + ", DOB=" + DOB
@@ -59,11 +70,13 @@ public class Passenger {
 				+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
 				+ "]";
 	}
-		public void addReservation(Reservation reservation) {
+
+	public void addReservation(Reservation reservation) {
 		this.addReservation(reservation);
 	}
-	
-	
-	
+
+	public List<Reservation> getReservations() {
+		return reservations;
+	}
 
 }
